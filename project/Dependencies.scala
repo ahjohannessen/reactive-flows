a813a6028bdce86a9626ca7b249891b132fa03ca
Heiko Seeberger <mail@heikoseeberger.de>
2014-11-03 12:06:49 +0100

    Set up project

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,18 +1,29 @@
 import sbt._
 
 object Version {
-  val akka      = "2.3.6"
-  val logback   = "1.1.2"
-  val scala     = "2.11.2"
-  val scalaTest = "2.2.2"
-  val scalactic = "2.2.2"
+  val akka                 = "2.3.6"
+  val akkaDataReplication  = "0.8"
+  val akkaHttp             = "0.10.0-M2"
+  val akkaPersistenceMongo = "0.7.4"
+  val logback              = "1.1.2"
+  val scala                = "2.11.4"
+  val scalaTest            = "2.2.2"
+  val sprayJson            = "1.2.6"
 }
 
 object Library {
-  val akkaActor      = "com.typesafe.akka" %% "akka-actor"      % Version.akka
-  val akkaSlf4j      = "com.typesafe.akka" %% "akka-slf4j"      % Version.akka
-  val akkaTestkit    = "com.typesafe.akka" %% "akka-testkit"    % Version.akka
-  val logbackClassic = "ch.qos.logback"    %  "logback-classic" % Version.logback
-  val scalaTest      = "org.scalatest"     %% "scalatest"       % Version.scalaTest
-  val scalactic      = "org.scalactic"     %% "scalactic"       % Version.scalactic
+  val akkaActor            = "com.typesafe.akka"   %% "akka-actor"                    % Version.akka
+  val akkaContrib          = "com.typesafe.akka"   %% "akka-contrib"                  % Version.akka
+  val akkaDataReplication  = "com.github.patriknw" %% "akka-data-replication"         % Version.akkaDataReplication
+  val akkaHttp             = "com.typesafe.akka"   %% "akka-http-experimental"        % Version.akkaHttp
+  val akkaPersistenceMongo = "com.github.ddevore"  %% "akka-persistence-mongo-casbah" % Version.akkaPersistenceMongo
+  val akkaSlf4j            = "com.typesafe.akka"   %% "akka-slf4j"                    % Version.akka
+  val akkaTestkit          = "com.typesafe.akka"   %% "akka-testkit"                  % Version.akka
+  val logbackClassic       = "ch.qos.logback"      %  "logback-classic"               % Version.logback
+  val scalaTest            = "org.scalatest"       %% "scalatest"                     % Version.scalaTest
+  val sprayJson            = "io.spray"            %% "spray-json"                    % Version.sprayJson
+}
+
+object Resolver {
+  val patriknw = "patriknw at bintray" at "http://dl.bintray.com/patriknw/maven"
 }
